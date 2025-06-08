0a76536ef4c29e2e4ff39626d0610a70bccaa2c3
Walmik <walmik.deshpande@gmail.com>
2025-06-07 18:09:22 -0700

    Update GA script

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,10 +33,11 @@ export default function RootLayout({
         <Script id="gtag-init">
           {`
             if (window.location.hostname !== "localhost") {
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){ window.dataLayer.push(arguments); }
-              gtag("js", new Date());
-              gtag("config", "G-BB3L4GLBNT");
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PLSRNRN');
             }
           `}
         </Script>
